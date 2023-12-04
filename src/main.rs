8f809383f57eb71e2f1b62c0a96547e874b8ba20
AMBase <milnikovalexei@gmail.com>
2023-12-04 21:44:57 +0300

    feat: added router for signin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod config;
 use config::Config;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+#[derive(Serialize)]
+struct SignInRespData {
+    access_token: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -17,6 +22,13 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn signin() -> impl Responder {
+    let resp_data = SignInRespData {
+        access_token: "qwe.asd.zxc".to_string(),
+    };
+    web::Json(resp_data)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = Config::new();
@@ -26,6 +38,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/api/v1/auth/signin", web::post().to(signin))
     })
         .bind((config.server.host, config.server.port))?
         .run()
