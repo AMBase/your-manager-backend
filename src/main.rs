f81fe97d8383657115f3d479b2e445465fc0dfb4
AMBase <milnikovalexei@gmail.com>
2023-12-18 23:14:51 +0300

    feat: add postgres pool

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod config;
 use config::Config;
-
+use sqlx;
 use actix_web::{web, App, HttpServer, Responder};
 use serde::Serialize;
+
 #[derive(Serialize)]
 struct SignInRespData {
     access_token: String,
@@ -17,14 +18,28 @@ async fn signin() -> impl Responder {
     web::Json(resp_data)
 }
 
+async fn signup() -> impl Responder {
+    let resp_data = SignInRespData {
+        access_token: "qwe.asd.zxc".to_string(),
+    };
+    web::Json(resp_data)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = Config::new();
 
+    let conn_url = "postgres://postgres:postgres@localhost/postgres";
+    let pool = sqlx::PgPool::connect(&conn_url).await.unwrap();
+
+    println!("pool = {:?}", pool);
+
     println!("Server running on host {} port {}", config.server.host, config.server.port);
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .route("/api/v1/auth/signin", web::post().to(signin))
+            .route("/api/v1/auth/signup", web::post().to(signup))
     })
         .bind((config.server.host, config.server.port))?
         .run()
